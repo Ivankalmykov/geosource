9e4e9993eab1f72314a6388b54e3558cae8e05bc
ivankalmykov <Banu4@yandex.ru>
2020-06-02 12:16:37 +0400

    Apache logger replaced with default logger Lombok(Slf4j)

--- src/main/java/ru/geosource/service/OpenStreetMapService.java
+++ src/main/java/ru/geosource/service/OpenStreetMapService.java
@@ -1,6 +1,7 @@
 package ru.geosource.service;
 
 import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
@@ -8,11 +9,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import ru.geosource.dto.CountyDto;
 import ru.geosource.dto.StateDto;
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
+@Slf4j
 @Service
 public class OpenStreetMapService {
     @Autowired
@@ -25,11 +26,17 @@ public class OpenStreetMapService {
 
     public List<StateDto> findByState(@NonNull String state) {
         ResponseEntity<StateDto[]> stateEntity = restTemplate.getForEntity(urlForState, StateDto[].class, state);
+        if (Arrays.asList(Objects.requireNonNull(stateEntity.getBody())).isEmpty()){
+            log.error("Объект c названием " + state + " не найден");
+        }
         return Arrays.asList(Objects.requireNonNull(stateEntity.getBody()));
     }
 
     public List<CountyDto> findByCounty(@NonNull String county) {
         ResponseEntity<CountyDto[]> countyEntity = restTemplate.getForEntity(urlForCounty, CountyDto[].class, county);
+        if (Arrays.asList(Objects.requireNonNull(countyEntity.getBody())).isEmpty()){
+            log.error("Объект с названием " + county + " не найден");
+        }
         return Arrays.asList(Objects.requireNonNull(countyEntity.getBody()));
     }
 }
